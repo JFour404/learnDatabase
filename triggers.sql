3ab4cd9cbc617791d24a87d8682174313b56aea0
Justas Remeikis <justas00001@gmail.com>
2023-12-19 16:46:11 +0200

    more triggers

--- triggers.sql
+++ triggers.sql
@@ -8,6 +8,60 @@ BEGIN
 END;
 /
 
+--Tikrint asmens koda, ilgi, kad butu 11 simboliu ir kad butu tik skaiciai
+CREATE OR REPLACE TRIGGER patikrint_asmens_koda
+BEFORE INSERT OR UPDATE ON Naudotojas
+FOR EACH ROW
+BEGIN
+    IF LENGTH(:NEW.asmensKodas) != 11 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Netinkamas asmens kodas.');
+    END IF;
+    IF :NEW.asmensKodas NOT LIKE '___________' THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Netinkamas asmens kodas.');
+    END IF;
+END;
+
+--Tikrint ar sporto klubo plotas yra teigiamas
+CREATE OR REPLACE TRIGGER patikrint_sporto_klubo_plota
+BEFORE INSERT OR UPDATE ON SportoKlubas
+FOR EACH ROW
+BEGIN
+    IF :NEW.plotas < 0 THEN
+        RAISE_APPLICATION_ERROR(-20004, 'Sporto klubo plotas negali buti neigiamas.');
+    END IF;
+END;
+
+--Tikrint ar sporto zonos plotas yra teigiamas
+CREATE OR REPLACE TRIGGER patikrint_sporto_zonos_plota
+BEFORE INSERT OR UPDATE ON SportoZona
+FOR EACH ROW
+BEGIN
+    IF :NEW.plotas < 0 THEN
+        RAISE_APPLICATION_ERROR(-20005, 'Sporto zonos plotas negali buti neigiamas.');
+    END IF;
+END;
+
+--Tikrint ar trenerio darbo stazas yra teigiamas
+CREATE OR REPLACE TRIGGER patikrint_trenerio_darbo_staza
+BEFORE INSERT OR UPDATE ON Treneris
+FOR EACH ROW
+BEGIN
+    IF :NEW.darboStazas < 0 THEN
+        RAISE_APPLICATION_ERROR(-20006, 'Trenerio darbo stazas negali buti neigiamas.');
+    END IF;
+END;
+
+--Tikrint ar abonemento planas yra terminuotas
+CREATE OR REPLACE TRIGGER patikrint_abonemento_plano_termina
+BEFORE INSERT OR UPDATE ON AbonementoPlanas
+FOR EACH ROW
+BEGIN
+    IF :NEW.arTerminuota NOT IN (0, 1) THEN
+        RAISE_APPLICATION_ERROR(-20007, 'Abonemento planas turi buti terminuotas arba neterminuotas.');
+    END IF;
+END;
+
+
 CREATE OR REPLACE TRIGGER cascade_istrinamas_sporto_planas
 BEFORE DELETE ON SportoPlanas
 FOR EACH ROW
